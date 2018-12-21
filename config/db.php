1577a333ed94b8623e776a4c725736e1ccb24f7f
Sunny Pepple <sunny.pepple@tonyelumelufoundation.org>
2018-12-21 16:15:52 +0100

    Folder update

--- config/db.php
+++ config/db.php
@@ -2,8 +2,17 @@
 /*
  * Connect to the Database.
  */
-
-CONST
+CONST HOST = localhost;
 CONST DATABASE_NAME = 'business';
 CONST USER_NAME = 'slpepple01';
 CONST PASSWORD = "1";
+
+
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=business',USER_NAME,PASSWORD);
+    $db;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}

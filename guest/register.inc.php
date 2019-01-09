c7e6cc639ee6f978544999427151e5a140022df0
Ola Eleyinmi <eleyinmiolasunkanmi@gmail.com>
2019-01-09 19:30:36 +0100

    Added a register.css file

--- guest/register.inc.php
+++ guest/register.inc.php
@@ -1,10 +1,52 @@
-<?
-?>
-
 <!DOCTYPE html>
 <html>
-<head></head>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Business Plus | Register</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="register.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
 <body>
 
+    <form>
+
+        <div class="field">
+        <label for="name" id="name">Your Name</label>
+        <input type="text" name="name" id="theName" class="input"/>
+        </div>
+        <br/><br/>
+        <div>
+        <label for="email" id="email">Email</label>
+        <input type="email" name="email" id="theEmail" class="input"/>
+        </div>
+        <br/><br/>
+        <div>
+        <label for="password" id="password">Password</label>
+        <input type="password" name="password" id="thePassword" class="input"/>
+        </div>
+        <br/><br/>
+        <input type="submit" value="Sign up"/>
+    </form>
+
+<script>
+$(document).ready(function(){
+    $("#theName").focus(function(){
+        $("#name").animate({
+            color: '#622670',
+            fontSize: '13px'
+        });
+        $(this).animate({borderBottom: '2px solid #622670'});
+    });
+    $("#theName").blur(function(){
+        $("#name").animate({
+            color: 'grey',
+            fontSize: '1.2em'
+        });
+        $(this).animate({borderBottom: '2px solid grey'});
+    });
+});
+</script>
 </body>
 </html>
